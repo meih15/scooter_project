98de82a79a5bd0132c3add49383a5d66f3ba7db3
Mei Huang <meihuang8328@gmail.com>
2024-04-24 15:25:04 -0700

    added user class with login and logout functionality

---
+++ src/User.js
@@ -0,0 +1,23 @@
+class User {
+
+    constructor(username, password, age) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.loggedIn = false;
+    }
+
+    login(password) {
+        if (password === this.password) {
+            this.loggedIn = true;
+        } else {
+            throw new Error("incorrect password")
+        }
+    }
+
+    logout() {
+        this.loggedIn = false;
+    }
+}
+
+module.exports = User;
